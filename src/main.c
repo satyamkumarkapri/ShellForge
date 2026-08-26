a0a9e812061b1e38730dc4bbe6b9ed926e641511
satyamkumarkapri <satyamkumarkapri17@gmail.com>
2026-08-26 17:19:15 +0530

    Builtin and executor

--- src/main.c
+++ src/main.c
@@ -1,59 +1,79 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <readline/history.h>
 #include <readline/readline.h>
+
+#include "history.h"
 #include "token.h"
 #include "lexer.h"
+#include "builtin.h"
 #include "parser.h"
 #include "expand.h"
+#include "executor.h"
+
 int main(void)
 {
-    // Display a welcome banner when the shell starts
     printf("=====================================\n");
-    printf("      Shellforge \n");
+    printf("      Shellforge\n");
     printf(" A Unix Style Shell written in C\n");
     printf("=====================================\n");
 
-  token_list_t tokens;
- pipeline_t pipeline;
- 
- char *line;
+    token_list_t tokens;
+    pipeline_t pipeline;
+
+    char *line;
 
     while (1)
     {
         line = readline("shellforge$ ");
+
         if (line == NULL)
         {
             printf("\nGoodbye!\n");
             break;
         }
+
         if (strlen(line) == 0)
         {
             free(line);
             continue;
         }
 
-         add_history(line);
-	lexer(line, &tokens);
-        token_print(&tokens);
-        
-
-	if(parser(&tokens, &pipeline))
-	{
-		expand_variables(&pipeline);
-    		pipeline_print(&pipeline);
-	}
-        
-
-
-        if (strcmp(line, "exit") == 0)
+        /* History command */
+        if (strcmp(line, "history") == 0)
         {
+            print_history();
             free(line);
-            printf("Exiting...\n");
-            break;
+            continue;
         }
-	free(line);
-    }    
+
+        /* Add command to history */
+        add_history(line);
+
+        /* Lexer */
+        lexer(line, &tokens);
+
+        /* Parser and expansion */
+        if (parser(&tokens, &pipeline))
+        {
+            expand_variables(&pipeline);
+        }
+
+        /* Execute commands */
+        for (int i = 0; i < pipeline.command_count; i++)
+        {
+            int result = execute_command(&pipeline.commands[i]);
+
+            if (result == 1)
+            {
+                free(line);
+                return 0;
+            }
+        }
+
+        free(line);
+    }
+
     return 0;
 }
